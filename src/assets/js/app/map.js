90d81a36aa7d3b29095f0a1bde36d0498f9c95bb
Alex Saskevich <alexhomel@ya.ru>
2014-09-05 14:22:38 +0300

    Updated main page

--- src/assets/js/app/map.js
+++ src/assets/js/app/map.js
@@ -2,7 +2,7 @@ function initialize() {
     var mapProp = {
         center: new google.maps.LatLng(51.508742, -0.120850),
         zoom: 5,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeId: google.maps.MapTypeId.TERRAIN
     };
     var map = new google.maps.Map(document.getElementById("googleMap")
         , mapProp);
