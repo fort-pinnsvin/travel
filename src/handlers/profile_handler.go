ccc656b6cddc8b70d27166da0f13e17fb9e7c472
blackazaru <danik.cyrkunov@gmail.com>
2014-09-05 16:38:22 +0300

    fix some bug

--- src/handlers/profile_handler.go
+++ src/handlers/profile_handler.go
@@ -12,15 +12,26 @@ func UserProfile(rnd render.Render, params martini.Params, session sessions.Sess
 	if session.Get("auth_id") != "" {
 		id := params["id"]
 		user := models.User{}
+		user_auth := models.User{}
 		err := models.UserCollection.FindId(id).One(&user)
-
+		models.UserCollection.FindId(session.Get("auth_id")).One(&user_auth)
 		if err != nil {
 			fmt.Println(err)
 			rnd.Redirect("/")
 			return
 		}
+		rnd.HTML(200, "user", map[string]string{
+			"auth_first_name": user_auth.FirstName,
+			"auth_last_name": user_auth.LastName,
+			"auth_avatar": user_auth.Avatar,
+			"auth_id": user_auth.Id,
+			"first_name": user.FirstName,
+			"last_name": user.LastName,
+			"email": user.Email,
+			"avatar": user.Avatar,
 
-		rnd.HTML(200, "user", user)
+
+		})
 	} else {
 		rnd.HTML(200, "not_allowed", map[string]string{"error": "Not authorized"})
 	}
