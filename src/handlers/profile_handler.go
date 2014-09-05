a98853eb54a60f48840fdc63a91062d04d9640a8
blackazaru <danik.cyrkunov@gmail.com>
2014-09-05 16:44:19 +0300

    fix button edit

--- src/handlers/profile_handler.go
+++ src/handlers/profile_handler.go
@@ -20,6 +20,10 @@ func UserProfile(rnd render.Render, params martini.Params, session sessions.Sess
 			rnd.Redirect("/")
 			return
 		}
+		b := ""
+		if id == session.Get("auth_id") {
+			b = "true"
+		}
 		rnd.HTML(200, "user", map[string]string{
 			"auth_first_name": user_auth.FirstName,
 			"auth_last_name": user_auth.LastName,
@@ -29,8 +33,7 @@ func UserProfile(rnd render.Render, params martini.Params, session sessions.Sess
 			"last_name": user.LastName,
 			"email": user.Email,
 			"avatar": user.Avatar,
-
-
+			"auth_user": b,
 		})
 	} else {
 		rnd.HTML(200, "not_allowed", map[string]string{"error": "Not authorized"})
