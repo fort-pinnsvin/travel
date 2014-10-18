db5609803fa83d61263f2a8ed4f39f395aa85e15
Alex Saskevich <alexhomel@ya.ru>
2014-10-18 18:23:40 +0300

    Easy solution for creating new markers

--- handlers/root_handler.go
+++ handlers/root_handler.go
@@ -2,10 +2,13 @@ package handlers
 
 import (
 	"github.com/fort-pinnsvin/travel/models"
+	"github.com/fort-pinnsvin/travel/utils"
 	"github.com/martini-contrib/oauth2"
 	"github.com/martini-contrib/render"
 	"github.com/martini-contrib/sessions"
 	"net/http"
+	"time"
+	"fmt"
 )
 
 func Root(tokens oauth2.Tokens, rnd render.Render, r *http.Request, session sessions.Session) {
@@ -17,6 +20,34 @@ func Root(tokens oauth2.Tokens, rnd render.Render, r *http.Request, session sess
 		user.LastName = session.Get("last_name").(string)
 		user.Id = session.Get("auth_id").(string)
 		user.Avatar = session.Get("avatar").(string)
+		if r.FormValue("address") != "" {
+			println(r.FormValue("address"))
+			lat, lng := GetLatLngByAddress(tokens, r.FormValue("address"))
+			marker := &models.Marker{}
+			marker.Id = models.GenerateId()
+			marker.Owner = session.Get("auth_id").(string)
+			marker.Name = r.FormValue("address")
+			marker.Latitude = fmt.Sprintf("%0.8f", lat)
+			marker.Longitude = fmt.Sprintf("%0.8f", lng)
+			marker.Description = ""
+			marker.FullAddress = "http://placehold.it/250x130"
+			marker.Date = time.Now().Format(models.Layout)
+			marker.Nano = time.Now().Unix()
+			models.MarkerCollection.Insert(&marker)
+			// Add marker to feed
+			new_post := models.Post{}
+			new_post.Id = models.GenerateId()
+			new_post.Owner = session.Get("auth_id").(string)
+			new_post.Text = `<img src="http://placehold.it/250x130"/><br/>Watch it <a href="` +
+					"//" +utils.GetValue("WWW", "localhost:3000") + "/album/" +marker.Id+"/"+ `">here</a>.`
+			new_post.Title = "I create New Album!"
+			new_post.Date = time.Now().Format(models.Layout)
+			new_post.Nano = time.Now().Unix()
+			models.PostCollection.Insert(&new_post)
+
+			GetCountry(tokens, marker.Latitude, marker.Longitude)
+			rnd.Redirect("/")
+		}
 		rnd.HTML(200, "home_user", user)
 	}
 }
